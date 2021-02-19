d182a4893b797fd66f62dbd4a5c36d35e60abfaa
ramonmarrero <ramon.marrero@lanube.io>
2021-02-19 16:55:21 +0100

    aws-cdk-intro setup

--- aws-cdk-intro/aws-cdk.sh
+++ aws-cdk-intro/aws-cdk.sh
@@ -1,20 +1,23 @@
 #Install Python 3.7
-#sudo apt update
-#sudo apt install software-properties-common
-#sudo add-apt-repository ppa:deadsnakes/ppa
-#sudo apt install python3.7 -y
+sudo apt update
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt install python3.7 -y
 
 #Install AWS CDK 
-#npm install -g aws-cdk
+npm install -g aws-cdk
 
 #Create directory
-#mkdir aws-cdk-intro
-#cd aws-cdk-intro
+mkdir aws-cdk-intro
+cd aws-cdk-intro
 
 #Initialize AWS CDK
-#cdk init app --language python
+cdk init app --language python
 
-#Activate the environment
-#source .venv/bin/activate
-#python -m pip install -r requirements.txt
+#Install dependecies
+sudo apt install python3-pip -y
+python3.7 -m pip install -r requirements.txt
+
+#Display Stack
+cdk ls
 
