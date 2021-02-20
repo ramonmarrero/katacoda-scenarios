0c3aeac884283a3f6a0364ebde7f4a6d71a11b99
ramonmarrero <ramon.marrero@lanube.io>
2021-02-20 09:15:52 +0100

    setting aws cdk with vscode

--- aws-cdk-intro/aws-cdk.sh
+++ aws-cdk-intro/aws-cdk.sh
@@ -1,1 +1,5 @@
 
+sudo apt update; sudo apt install software-properties-common -y; sudo add-apt-repository ppa:deadsnakes/ppa -y; sudo apt install python3.7 -y;
+sudo apt install python3-pip -y; sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 1;
+
+
